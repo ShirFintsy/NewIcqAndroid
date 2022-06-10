be01de36f55c8f33cd45f5ca413f8db123f7fd4d
shirf <shir>
2022-06-10 10:00:04 +0300

    tries some changes

---
+++ app/src/main/java/com/example/newicqandroid/ChatsActivity.java
@@ -0,0 +1,22 @@
+package com.example.newicqandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.newicqandroid.databinding.ActivityChatsBinding;
+import com.example.newicqandroid.databinding.ActivityLoginBinding;
+
+public class ChatsActivity extends AppCompatActivity {
+    private ActivityChatsBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Intent intent = getIntent();
+        String username = intent.getExtras().getString("username");
+        binding.currentUser.setText(username);
+    }
+}
--- app/src/main/java/com/example/newicqandroid/LogInActivity.java
+++ app/src/main/java/com/example/newicqandroid/LogInActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
 import com.example.newicqandroid.databinding.ActivityLoginBinding;
 
 public class LogInActivity extends AppCompatActivity {
@@ -17,9 +19,16 @@ public class LogInActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setListeners();
     }
-
     private void setListeners(){
         binding.btnLogin.setOnClickListener(v->
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class)));
     }
+//    private void setListeners(){
+//        binding.btnLogin.setOnClickListener(v-> {
+//            EditText username = findViewById(R.id.userNameInput);
+//            Intent intent = new Intent(this, ChatsActivity.class);
+//            intent.putExtra("username", username.getText().toString());
+//            startActivity(intent);
+//        });
+//    }
 }
