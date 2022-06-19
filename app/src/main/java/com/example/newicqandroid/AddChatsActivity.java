b7c101ba89078a7497917a12bbc0954c49fba763
shirf <shir>
2022-06-19 14:41:21 +0300

    change some design

--- app/src/main/java/com/example/newicqandroid/AddChatsActivity.java
+++ app/src/main/java/com/example/newicqandroid/AddChatsActivity.java
@@ -1,15 +1,19 @@
 package com.example.newicqandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.newicqandroid.api.ApiManager;
 import com.example.newicqandroid.databinding.ActivityAddChatsBinding;
 import com.example.newicqandroid.databinding.ActivityChatsBinding;
+import com.example.newicqandroid.entities.User;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class AddChatsActivity extends AppCompatActivity {
+public class AddChatsActivity extends AppCompatActivity{
     private ActivityAddChatsBinding binding;
+    private ApiManager apiManager = new ApiManager();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,7 @@ public class AddChatsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         binding.addButton.setOnClickListener(view-> {
             String user = binding.addChatInput.getText().toString();
+
             Intent toIntent = new Intent(getApplicationContext(), ChatsActivity.class);
             toIntent.putExtra("username", user);
             setResult(RESULT_OK, toIntent);
@@ -24,7 +29,5 @@ public class AddChatsActivity extends AppCompatActivity {
         });
 
     }
-
-
 
 }
